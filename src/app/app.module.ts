526376955f4490408e2133fa212e994d59ba218c
Tutoleapp <ragonzalezr@bt.unal.edu.co>
2018-08-30 09:24:21 -0500

    Mensaje

---
+++ src/app/app.module.ts
@@ -0,0 +1,52 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+
+/**
+ * Librerias FILEBASE AUTEN
+ * 
+ */
+import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../environments/environment';
+
+
+
+import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
+import { ItemDetailsPage } from '../pages/item-details/item-details';
+import { ListPage } from '../pages/list/list';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  
+  declarations: [
+    MyApp,
+    HelloIonicPage,
+    AppComponent,
+    ItemDetailsPage,
+    ListPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(environment.firebase)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HelloIonicPage,
+    ItemDetailsPage,
+    AppComponent,
+    ListPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyDrRE6wISrGd0JeTp1JJRAgceTxUx-slD8",
+        authDomain: "myionicproject-2201b.firebaseapp.com",
+        databaseURL: "https://myionicproject-2201b.firebaseio.com",
+        projectId: "myionicproject-2201b",
+        storageBucket: "myionicproject-2201b.appspot.com",
+        messagingSenderId: "859452895172"
+    }
+  };
